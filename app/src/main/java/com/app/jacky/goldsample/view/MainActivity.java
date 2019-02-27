34e9f2d8fbc1268fc3708e6fdac0a474f7fbd5d0
jacky <1165002248@qq.com>
2019-02-27 16:53:52 +0800

    Add ble bluetooth test code for bracelet by jacky 2019/2/27.

--- app/src/main/java/com/app/jacky/goldsample/view/MainActivity.java
+++ app/src/main/java/com/app/jacky/goldsample/view/MainActivity.java
@@ -1,23 +1,32 @@
 package com.app.jacky.goldsample.view;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.app.jacky.goldsample.R;
-import com.app.jacky.goldsample.entity.User;
+import com.app.jacky.goldsample.application.Utils;
 import com.app.jacky.goldsample.logger.LoggerUtil;
-import com.app.jacky.goldsample.retrofit.RetrofitHelper;
-import com.app.jacky.goldsample.service.UserService;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.guanaj.easyswipemenulibrary.EasySwipeMenuLayout;
@@ -26,50 +35,64 @@ import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Action;
+import butterknife.OnClick;
 import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.tv_title)
-    TextView tvTitle;
+
+    private static final int SERVICE_BIND = 0X0001;
+    private static final int CONNECTING = 0X0002;
+    private static final int CONNECTED = 0X0003;
+    private static final int DISCONNECTED = 0X0004;
+    private static final int DISCONNECTING = 0X0005;
     @BindView(R.id.rv_easy_swipe)
     RecyclerView rvEasySwipe;
+
+    @BindView(R.id.bt_bind_service)
+    Button btBindService;
+
+    @BindView(R.id.bt_enable)
+    Button btEnable;
+
+    @BindView(R.id.bt_scan)
+    Button btScan;
+
+    @BindView(R.id.bt_connect)
+    Button btConnect;
+
+    @BindView(R.id.bt_read)
+    Button btRead;
+
+    @BindView(R.id.bt_write)
+    Button btWrite;
 
     private RvEasySwipeAdapter rvAdapter;
     private List<String> listData;
     private LayoutInflater inflater;
 
+
+    private static final String SMART_WATCH_MAC="00:00:18:7A:93:01";
+
+    private static final String SERVICE_UUID="0000fff0-0000-1000-8000-00805f9b34fb";
+    private static final String NOTIFY_CHARACTERISTIC_UUID="0000fff1-0000-1000-8000-00805f9b34fb";
+    private static final String WRITE_CHARACTERISTIC_UUID="0000fff2-0000-1000-8000-00805f9b34fb";
+    private static final String CLIENT_CHARACTERISTIC_CONFIG="00002902-0000-1000-8000-00805F9B34FB";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
         initData();
-
-        tvTitle.setText("GoldSample");
-        LoggerUtil.d("debug>>>>>>");
-        LoggerUtil.i("info>>>>>>");
-        LoggerUtil.w("warning>>>>>>");
-        LoggerUtil.v("verbose>>>>>>");
-        LoggerUtil.wtf("what a terrible failure>>>>>>");
-
         RxPermissions rxPermissions = new RxPermissions(this);
 
         rxPermissions.
-                requestEachCombined(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestEachCombined(Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.BLUETOOTH,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
                 .subscribe(new Consumer<Permission>() {
                     @Override
                     public void accept(Permission permission) throws Exception {
@@ -83,34 +106,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        Observable.create(new ObservableOnSubscribe<Integer>() {
-            @Override
-            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
-                emitter.onNext(123);
-                sleep(3000);
-                emitter.onNext(456);
-            }
-        }).observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(new Consumer<Integer>() {
-                               @Override
-                               public void accept(Integer integer) throws Exception {
-                                   Log.d("MainActivity", "Accept:" + integer);
-                               }
-
-                           },
-                        new Consumer<Throwable>() {
-                            @Override
-                            public void accept(Throwable throwable) throws Exception {
-
-                            }
-                        },
-                        new Action() {
-                            @Override
-                            public void run() throws Exception {
-
-                            }
-                        });
+        initBle();
     }
 
     private void initView()
@@ -122,12 +118,11 @@ public class MainActivity extends AppCompatActivity {
         inflater = getLayoutInflater();
     }
 
+
+
     private void initData()
     {
         listData = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            listData.add("index is =" + i);
-        }
         rvAdapter.addData(listData);
         rvAdapter.notifyDataSetChanged();
     }
@@ -137,11 +132,13 @@ public class MainActivity extends AppCompatActivity {
 
         public RvEasySwipeAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
             super(layoutResId, data);
+
         }
 
         @Override
         protected void convert(final BaseViewHolder helper, String item) {
 
+            helper.setText(R.id.tv_content,item);
             helper.getView(R.id.right_menu_2).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -154,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             helper.getView(R.id.content).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "setOnClickListener", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "setOnClickListener" + v.toString(), Toast.LENGTH_SHORT).show();
 
                 }
             });
@@ -162,24 +159,212 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Retrofit test
-    private void getUserDetail()
+    private Handler mHandler = new Handler()
     {
-        UserService request = RetrofitHelper.getInstance(this).getRetrofitAPI(UserService.class);
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what)
+            {
+                case SERVICE_BIND:
+                    btBindService.setText("UNBAND");
+                    break;
+                case DISCONNECTED:
+                    btConnect.setText("CONNECT");
+                    mIsConnected = false;
+                    mBluetoothDevice = mBluetoothAdapter.getRemoteDevice(SMART_WATCH_MAC);
+                    connect();
+                    break;
+                case DISCONNECTING:
+                    btConnect.setText("DISCONNECTING");
+                    break;
+                case CONNECTING:
+                    btConnect.setText("CONNECTING");
+                    break;
+                case CONNECTED:
+                    btConnect.setText("CONNECTED");
+                    mIsConnected = true;
 
-        Call<User> call = request.getUserByName("jacky");
 
-        call.enqueue(new Callback<User>(){
+                    mBluetoothGatt.discoverServices();
 
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
+                    break;
+            }
+        }
+    };
 
+    public void setCharacteristicNotification(BluetoothGattCharacteristic characteristic, boolean enabled) {
+
+        if (mBluetoothAdapter == null || mBluetoothGatt == null) {
+            return;
+        }
+        //通知远程端开启 notify
+        if (characteristic.getDescriptor(UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG)) != null) {
+            if (enabled == true) {
+                BluetoothGattDescriptor descriptor = characteristic
+                        .getDescriptor(UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG));
+                descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                mBluetoothGatt.writeDescriptor(descriptor);
+            } else {
+                BluetoothGattDescriptor descriptor = characteristic
+                        .getDescriptor(UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG));
+                descriptor.setValue(BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE);
+                mBluetoothGatt.writeDescriptor(descriptor);
+            }
+        }
+        mBluetoothGatt.setCharacteristicNotification(characteristic, enabled);
+    }
+
+    private boolean mIsBind = false;
+    private boolean mIsScanning = false;
+    private boolean mIsConnected = false;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothGatt mBluetoothGatt;
+    private BluetoothDevice mBluetoothDevice;
+    private BluetoothGattService mBluetoothGattService;
+
+    private BluetoothGattCharacteristic mNotifyCharacteristic;
+    private BluetoothGattCharacteristic mWriteCharacteristic;
+
+    private void initBle()
+    {
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            mBluetoothAdapter.enable();
+        }
+    }
+
+    private final  BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+
+            if(mBluetoothGatt == null)
+            {
+                LoggerUtil.d("Bluetooth gatt is null!");
+                return;
+            }
+            mBluetoothGattService = mBluetoothGatt.getService(UUID.fromString(SERVICE_UUID));
+            if(mBluetoothGattService == null)
+            {
+                LoggerUtil.d("Bluetooth gatt service is null!");
+                return;
             }
 
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
+            mNotifyCharacteristic = mBluetoothGattService.getCharacteristic(UUID.fromString(NOTIFY_CHARACTERISTIC_UUID));
 
+            if(mNotifyCharacteristic != null)
+            {
+                setCharacteristicNotification(mNotifyCharacteristic,true);
+
+                LoggerUtil.d("Get notify characteristic success!");
             }
-        });
+
+            mWriteCharacteristic = mBluetoothGattService.getCharacteristic(UUID.fromString(WRITE_CHARACTERISTIC_UUID));
+
+            if(mWriteCharacteristic != null)
+            {
+                LoggerUtil.d("Get write characteristic success!");
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+            LoggerUtil.d("Characteristic read:" + Utils.bytesToHexStringNoSpace(characteristic.getValue()));
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+            LoggerUtil.d("Characteristic write:" + Utils.bytesToHexStringNoSpace(characteristic.getValue()));
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+            LoggerUtil.d("Characteristic change:" + Utils.bytesToHexStringNoSpace(characteristic.getValue()));
+        }
+
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status,
+                                            int newState) {
+
+            Message msg = new Message();
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                LoggerUtil.d("Connected!");
+                msg.what = CONNECTED;
+                mHandler.sendMessage(msg);
+            }
+            // GATT Server disconnected
+            else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                LoggerUtil.d("Disconnected!");
+                msg.what = DISCONNECTED;
+                mHandler.sendMessage(msg);
+            }
+            // GATT Server disconnected
+            else if (newState == BluetoothProfile.STATE_DISCONNECTING) {
+                LoggerUtil.d("Disconnecting!");
+                msg.what = DISCONNECTING;
+                mHandler.sendMessage(msg);
+            }
+            else if (newState == BluetoothProfile.STATE_CONNECTING) {
+                LoggerUtil.d("Connecting!");
+                msg.what = CONNECTING;
+                mHandler.sendMessage(msg);
+            }
+        }
+    };
+
+    @OnClick(R.id.bt_bind_service) void onBindService()
+    {
+
+    }
+
+    @OnClick(R.id.bt_scan) void onSanBle()
+    {
+
+    }
+
+    @OnClick(R.id.bt_connect) void onBleConnect()
+    {
+        if(!mIsConnected)
+        {
+            connect();
+        }
+        else
+        {
+            mBluetoothGatt.disconnect();
+        }
+    }
+
+    private void connect()
+    {
+        mBluetoothDevice = mBluetoothAdapter.getRemoteDevice(SMART_WATCH_MAC);
+        if(mBluetoothDevice == null)
+        {
+            LoggerUtil.d("Get remote device failed!");
+            return;
+        }
+        mBluetoothGatt = mBluetoothDevice.connectGatt(this,false,mGattCallback);
+    }
+
+    @OnClick(R.id.bt_write) void onBleWrite()
+    {
+        byte[] readStep = new byte[]{(byte)0xc6,0x01,0x08,(byte)0x08};
+
+        byte[] setTime = new byte[]{
+                (byte)0xc2,0x07,0x13,0x02,0x1b,0x0f,0x00,0x00,0x03,(byte)0x06,
+        };
+
+        mWriteCharacteristic.setValue(readStep);
+        boolean ret = mBluetoothGatt.writeCharacteristic(mWriteCharacteristic);
+        if(ret)
+            LoggerUtil.d("Write characteristic success!");
     }
 }
